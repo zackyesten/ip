370444e602aa5097398113197735a335e5c868d8
zackyesten <zackyesten@gmail.com>
2026-08-21 15:33:11 +0800

    Complete Level-0

---
+++ src/main/java/Zack.java
@@ -0,0 +1,12 @@
+public class Zack {
+    public static void main(String[] args) {
+        String line = "____________________________________________________________";
+
+        System.out.println(line);
+        System.out.println(" Hello! I'm Zack");
+        System.out.println(" What can I do for you?");
+        System.out.println(line);
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println(line);
+    }
+}
