25e3f27b56ff8040d407c0918c726986ab38dd85
zackyesten <zackyesten@gmail.com>
2026-08-28 15:40:56 +0800

    Add task storage and listing

--- src/main/java/Zack.java
+++ src/main/java/Zack.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class Zack {
     public static void main(String[] args) {
         String line = "____________________________________________________________";
+        String[] tasks = new String[100];
+        int taskCount = 0;
         Scanner scanner = new Scanner(System.in);
 
         System.out.println(line);
@@ -13,12 +15,23 @@ public class Zack {
         while (true) {
             String command = scanner.nextLine();
             System.out.println(line);
+
             if (command.equals("bye")) {
                 System.out.println(" Bye. Hope to see you again soon!");
                 System.out.println(line);
                 break;
+            } else if (command.equals("list")) {
+                System.out.println(" Here are the tasks in your list:");
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                }
+            } else {
+                tasks[taskCount] = command;
+                taskCount++;
+                System.out.println(" added: " + command);
+                System.out.println(" Now you have " + taskCount + " tasks in the list.");
             }
-            System.out.println(" " + command);
+
             System.out.println(line);
         }
     }
