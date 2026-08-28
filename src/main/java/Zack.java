b89344129d9aadf1e69987c5b096003e51dacb1a
zackyesten <zackyesten@gmail.com>
2026-08-28 15:36:53 +0800

    Add command echo support

--- src/main/java/Zack.java
+++ src/main/java/Zack.java
@@ -1,12 +1,25 @@
+import java.util.Scanner;
+
 public class Zack {
     public static void main(String[] args) {
         String line = "____________________________________________________________";
+        Scanner scanner = new Scanner(System.in);
 
         System.out.println(line);
         System.out.println(" Hello! I'm Zack");
         System.out.println(" What can I do for you?");
         System.out.println(line);
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println(line);
+
+        while (true) {
+            String command = scanner.nextLine();
+            System.out.println(line);
+            if (command.equals("bye")) {
+                System.out.println(" Bye. Hope to see you again soon!");
+                System.out.println(line);
+                break;
+            }
+            System.out.println(" " + command);
+            System.out.println(line);
+        }
     }
 }
